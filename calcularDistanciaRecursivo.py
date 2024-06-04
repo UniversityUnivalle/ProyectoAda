3329f12f2e54345f638003794dbcbc5dfde80a50
JALZ <jannus.largo@correounivalle.edu.co>
2024-06-04 15:08:06 -0500

    Se quitaron los comentarios de la logica del programa

--- calcularDistanciaRecursivo.py
+++ calcularDistanciaRecursivo.py
@@ -1,49 +1,6 @@
-# Propósito de la Función
-# La función calcularDistancia tiene como objetivo calcular la distancia euclidiana entre dos puntos en un plano cartesiano bidimensional
-# Los puntos están definidos por sus coordenadas (x1, y1) (x2, y2).
-
-# La función devuelve la distancia entre los dos puntos
-
-# Parámetros de Entrada
-# x1, y1: Coordenadas del primer punto
-# x2, y2: Coordenadas del segundo punto
-
 def calcularDistancia(x1, y1, x2, y2):
     distancia = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
     return distancia
-
-
-# Se eleva la distancia al cuadrado a la potencia 0.5 por la siguientes razon:
-# Equivalencia matemática:
-# Elevar un número al cuadrado y luego a la potencia 0.5 es equivalente a calcular su raíz cuadrada. Esto se debe a la propiedad de las potencias:
-
-# (a^n)^m = a^(n*m)
-# 
-# En este caso, a representa la distancia al cuadrado ((x2 - x1)^2 + (y2 - y1)^2), n es 2 y m es 0.5. Por lo tanto: 
-
-# (a^2)^0.5 = a^(2*0.5) = a
-
-# -------------------------------------
-# Propósito:
-# calcularDistanciasRecursivo es una función recursiva que calcula todas las distancias entre puntos dados en una lista de puntos
-
-# Parámetros:
-# puntos: Lista de puntos. Cada punto está representado por una tupla (x, y)
-# i, j: Índices que representan los puntos entre los cuales se está calculando la distancia
-# distancias: Lista que almacena las distancias calculadas
-
-# Inicialización:
-# distancias = None: Si distancias no se proporciona al llamar la función, se inicializa como una lista vacía
-
-# Condiciones de Terminación:
-# i >= len(puntos) - 1: Cuando i es igual o mayor a len(puntos) - 1, significa que se han calculado las distancias para todos los pares posibles de puntos, por lo que la función retorna distancias
-# j >= len(puntos): Cuando j es igual o mayor a len(puntos), se pasa al siguiente punto en i y se reinicia j a i + 2 para evitar calcular la distancia entre un punto y sí mismo y para evitar duplicados
-
-# Cálculo de Distancia:
-# distancias.append(...): Calcula la distancia entre los puntos puntos[i] y puntos[j] utilizando la función calcularDistancia, y añade el resultado a la lista distancias
-
-# Llamada Recursiva:
-# return calcularDistanciasRecursivo(puntos, i, j + 1, distancias): Llama recursivamente a la función con j incrementado en 1 para calcular la siguiente distancia
 
 def calcularDistanciasRecursivo(puntos, i=0, j=1, distancias=None):
     if distancias is None:
--- rectangulo.py
+++ rectangulo.py
@@ -1,34 +1,10 @@
 from calcularDistanciaRecursivo import calcularDistanciasRecursivo,calcularDistancia
 
-
-# Propósito:
-# areaRectangulo calcula el área de un rectángulo a partir de los puntos dados
-
-# Parámetros:
-# puntos: Lista de puntos que definen los vértices del rectángulo
-
-# Cálculo del Área:
-# lado1 = calcularDistancia(puntos[0][0], puntos[0][1], puntos[1][0], puntos[1][1]): Calcula la distancia entre los primeros dos puntos para obtener el primer lado del rectángulo
-# lado2 = calcularDistancia(puntos[1][0], puntos[1][1], puntos[2][0], puntos[2][1]): Calcula la distancia entre el segundo y tercer punto para obtener el segundo lado del rectángulo
-# return lado1 * lado2: Retorna el área del rectángulo, que es el producto de los dos lados calculados
 def areaRectangulo(puntos):
     lado1 = calcularDistancia(puntos[0][0], puntos[0][1], puntos[1][0], puntos[1][1])
     lado2 = calcularDistancia(puntos[1][0], puntos[1][1], puntos[2][0], puntos[2][1])
     return lado1 * lado2
-
-
-# Propósito:
-# esRectangulo verifica si los puntos dados forman un rectángulo
-
-# Parámetros:
-# puntos: Lista de puntos que definen los vértices del cuadrilátero
-
-# Verificación:
-# if len(puntos) != 4:: Si la cantidad de puntos no es igual a 4, retorna False porque no se puede formar un rectángulo con menos o más de cuatro puntos
-# distancias = calcularDistanciasRecursivo(puntos): Calcula todas las distancias entre los puntos utilizando la función calcularDistanciasRecursivo
-# distancias.sort(): Ordena las distancias de menor a mayor
-# return distancias[0] == distancias[1] and distancias[2] == distancias[3]: Retorna True si las distancias más cortas 
-# (los dos primeros y los dos últimos elementos de distancias después de ordenar) son iguales, lo que indica que el cuadrilátero es un rectángulo
+\
 def esRectangulo(puntos):
     if len(puntos) != 4:
         return False
@@ -36,6 +12,6 @@ def esRectangulo(puntos):
     distancias = calcularDistanciasRecursivo(puntos)
 
     distancias.sort()
-    # Rectángulo tiene 2 pares de lados iguales y 2 diagonales iguales
+    
     return (distancias[0] == distancias[1] and distancias[2] == distancias[3] and 
             distancias[4] == distancias[5])
