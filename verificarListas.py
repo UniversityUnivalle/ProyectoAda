b88d8ac0e84ada962c0c816d9353f50a922387f4
Ricardo <renatiko.04@gmail.com>
2024-06-08 15:57:52 -0500

    verficarListas
    Se modificó la función de verificar listas, para omitir el for que tenía y añadirlo de manera recursiva, con ello para ser menos tedioso a la hora de los costos.

--- verificarListas.py
+++ verificarListas.py
@@ -1,8 +1,24 @@
+# def verificarListas(arrayListas):
+#     listasValidas = []
+#     for lista in arrayListas:
+#         if len(lista) >= 3:
+#             listasValidas.append(lista)
+#         else:
+#             print(f"Lista {lista} no tiene suficientes puntos.")
+#     return listasValidas
+
 def verificarListas(arrayListas):
     listasValidas = []
-    for lista in arrayListas:
+
+    i = 0
+    while i < len(arrayListas):
+        lista = arrayListas[i]
+
         if len(lista) >= 3:
             listasValidas.append(lista)
         else:
             print(f"Lista {lista} no tiene suficientes puntos.")
+
+        i += 1
+
     return listasValidas
