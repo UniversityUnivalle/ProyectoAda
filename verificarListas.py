699b8ac6d11371fd5efaa94e00dadda0ff4dd3bc
Ricardo <renatiko.04@gmail.com>
2024-06-08 16:35:52 -0500

    update_verificarListas

--- verificarListas.py
+++ verificarListas.py
@@ -7,18 +7,34 @@
 #             print(f"Lista {lista} no tiene suficientes puntos.")
 #     return listasValidas
 
-def verificarListas(arrayListas):
-    listasValidas = []
+# def verificarListas(arrayListas):
+#     listasValidas = []
+    
+#     i = 0
+#     while i < len(arrayListas):
+#         lista = arrayListas[i]
 
-    i = 0
-    while i < len(arrayListas):
-        lista = arrayListas[i]
+#         if len(lista) >= 3:
+#             listasValidas.append(lista)
+#         else:
+#             print(f"Lista {lista} no tiene suficientes puntos.")
 
-        if len(lista) >= 3:
-            listasValidas.append(lista)
-        else:
-            print(f"Lista {lista} no tiene suficientes puntos.")
+#         i += 1
 
-        i += 1
+#     return listasValidas
 
-    return listasValidas
+def verificarListas(arrayListas, i=0, listasValidas=None):
+    if listasValidas is None:
+        listasValidas = []
+
+    if i >= len(arrayListas):
+        return listasValidas
+
+    lista = arrayListas[i]
+
+    if len(lista) >= 3:
+        listasValidas.append(lista)
+    else:
+        print(f"Lista {lista} no tiene suficientes puntos.")
+
+    return verificarListas(arrayListas, i + 1, listasValidas)
